9166ebb82699740bb63dbcf5ecae17e07c206b27
JM Revagorda <jmcr011504@gmail.com>
2025-09-20 17:56:44 +0800

    Update Admin.vb

--- IT-31-A Project/Admin.vb
+++ IT-31-A Project/Admin.vb
@@ -1,4 +1,6 @@
-﻿Public Class Admin
+﻿Imports IBM.Data.DB2
+Public Class Admin
+    Private dbconn As Common.DbConnection
 
     Private Sub Label13_Click(sender As Object, e As EventArgs)
 
@@ -9,6 +11,12 @@
     End Sub
 
     Private Sub Admin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        dbconn = New DB2Connection("server=localhost;database=cafeproj;" + "uid=db2admin;password=db2admin;")
+        dbconn.Open()
+
+    End Sub
+
+    Private Sub Load_Emp(Optional ByVal type As String = "")
 
     End Sub
 End Class
